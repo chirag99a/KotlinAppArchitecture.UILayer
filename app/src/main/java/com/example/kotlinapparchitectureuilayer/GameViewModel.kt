9de07f5829f5e558eb70be22bd9e9f8f9185ea21
chirag99a <cheerupapneja@gmail.com>
2021-02-11 10:11:17 +0530

    Kotlin App Architecture UI layer

---
+++ app/src/main/java/com/example/kotlinapparchitectureuilayer/GameViewModel.kt
@@ -0,0 +1,101 @@
+package com.example.kotlinapparchitectureuilayer
+
+import android.os.Build
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel(){
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get()= _score
+
+    private val _eventGameFinished= MutableLiveData<Boolean>()
+    val eventGameFinished: LiveData<Boolean>
+        get() = _eventGameFinished
+
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        _eventGameFinished.value = false
+        Log.i("GameViewModel", "GameViewModel created")
+        resetList()
+        nextWord()
+        _score.value= 0
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed")
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            _eventGameFinished.value= true
+            //gameFinished()
+        } else {
+            _word.value = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        _score.value= (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value= (score.value)?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinishComplete(){
+        _eventGameFinished.value = false
+    }
+}
